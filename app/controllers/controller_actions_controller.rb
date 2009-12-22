bf35c848530a8737eb5d41a12bf89e1d9a17399d
David Anderson <david@folklogic.com>
2009-12-22 17:56:06 +0100

    Oops. I should read the diffs before I commit them, shouldn't I?

--- app/controllers/controller_actions_controller.rb
+++ app/controllers/controller_actions_controller.rb
@@ -10,7 +10,6 @@ class ControllerActionsController < ApplicationController
     prepare_params
     re = /#{params[:controller_action][:page]}/i
     pages = @klazz.distinct_pages.select {|name| name =~ re}
-    puts pages.inspect
     modules = pages.map{|p| p =~ /^(.+?)::/ && $1 }.compact.uniq
     @completions = (pages + modules).sort
     render :inline => "<%= content_tag(:ul, @completions.map { |page| content_tag(:li, page) }) %>"
