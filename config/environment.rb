fdc277f6e41aa10b75b2b4d3c690c369a35b85fe
Stefan Kaes <skaes@railsexpress.de>
2010-08-06 12:12:08 +0200

    monkey patch for rack

--- config/environment.rb
+++ config/environment.rb
@@ -61,6 +61,15 @@ Rails::Initializer.run do |config|
     ActiveSupport::Dependencies.load_once_paths.reject! { |p| p =~ %r{/lib/} }
   end
 
+  # fix a bug in rack (more a brainfuck actually)
+  config.to_prepare do
+    Rack::Utils::HeaderHash.class_eval <<-_EVA_
+      def [](k)
+        super(@names[k] || @names[k.downcase])
+      end
+    _EVA_
+  end
+
   if GC.respond_to?(:log_file)
     config.to_prepare do
       # toggle GC tracing on signal USR1
