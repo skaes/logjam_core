2da2fd6420d715ff879469e85a347a3cfc68c4e8
Stefan Kaes <skaes@railsexpress.de>
2010-08-11 08:37:01 +0200

    no, we don't want it embedded right here

--- config/initializers/matchers.rb
+++ config/initializers/matchers.rb
@@ -1,14 +1,13 @@
 # CONFIGURATION
 #
-# Make sure to enable matchers appropriate for the log files that will be imported.
-# The sample log file included with LogJam is in basic time bandit format.
-# It is ok to have multiple COMPLETED matchers enabled; the first to match will be used.
 
 module Logjam
-  Logjam.base_url = 'stats'
+  # Logjam.base_url = 'stats'
 
+  # Make sure to enable matchers appropriate for the log files that will be imported.
+  # The sample log file included with LogJam is in basic time bandit format.
+  # It is ok to have multiple COMPLETED matchers enabled; the first to match will be used.
   RequestInfo.register_matcher Matchers::PROCESSING
   RequestInfo.register_matcher Matchers::SESSION_XING
-  # RequestInfo.register_matcher Matchers::COMPLETED_XING
-  RequestInfo.register_matcher Matchers::COMPLETED_TIME_BANDITS
+  RequestInfo.register_matcher Matchers::COMPLETED_XING
 end
