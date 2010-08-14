7c685015065285540961905ecea7e0eb04ac5308
David Anderson <david@folklogic.com>
2010-08-14 10:04:50 +0200

    Wish we had tests. Now I've been more thorough in my testing by hand.

--- app/models/logjam/requests.rb
+++ app/models/logjam/requests.rb
@@ -14,7 +14,6 @@ module Logjam
       query_opts = @options[:heap_growth_only] ? {"heap_growth" => {'$gt' => 0}} : {}
       query_opts.merge!(:response_code => @options[:response_code]) if @options[:response_code]
       query_opts.merge!(:page => /#{pattern}/) unless pattern.blank?
-      puts query_opts.inspect
       query_opts
     end
 
