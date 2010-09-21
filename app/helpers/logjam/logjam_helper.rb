7c38d20c2e7b13a49bba4b41a862094a209621c4
Stefan Kaes <skaes@railsexpress.de>
2010-09-21 08:10:19 +0200

    removed obsolete code

--- app/helpers/logjam/logjam_helper.rb
+++ app/helpers/logjam/logjam_helper.rb
@@ -125,29 +125,6 @@ module Logjam
       end
     end
 
-    def resource_descriptions
-      resources = Resource.time_resources + Resource.memory_resources + Resource.call_resources
-      groupings = Resource.groupings
-      functions = Resource.grouping_functions.reject(&:blank?)
-      g = {}
-      groupings.each do |grouping|
-        r = {}
-        resources.each do |resource|
-          if grouping.to_sym == :request || resource.to_sym == :requests
-            r[resource] = Resource.description(resource, grouping, :sum)
-          else
-            f = {}
-            functions.each do |function|
-              f[function] = Resource.description(resource, grouping, function)
-            end
-            r[resource] = f
-          end
-          g[grouping] = r
-        end
-      end
-      g.to_json
-    end
-
     SEVERITY_LABELS = %w(DEBUG INFO WARN ERROR FATAL)
 
     def format_severity(severity)
