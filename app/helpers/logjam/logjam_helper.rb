1f8e3d986c3b3e6eb4e3d826bda496f803d5e9f7
Stefan Kaes <stefan.kaes@xing.com>
2013-02-28 15:15:17 +0100

    show more of an error

--- app/helpers/logjam/logjam_helper.rb
+++ app/helpers/logjam/logjam_helper.rb
@@ -189,7 +189,7 @@ module Logjam
         error_line = safe_h(l.to_s)
         break if exception && error_line =~ regex
       end
-      error_line[0..70].html_safe
+      error_line[0..100].html_safe
     end
 
     def format_log_level(l)
