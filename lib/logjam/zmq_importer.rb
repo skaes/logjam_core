4f10fea2147e249f61e0cfe2a3f6f891fdcda006
Stefan Kaes <stefan.kaes@xing.com>
2013-03-20 17:35:51 +0100

    support event processing for zmq importer

--- lib/logjam/zmq_importer.rb
+++ lib/logjam/zmq_importer.rb
@@ -15,7 +15,8 @@ module Logjam
       @application  = @stream.app
       @environment  = @stream.env
       @context      = EM::ZeroMQ::Context.new(1)
-      @processor    = RequestProcessorServer.new(@stream, @context)
+      @request_processor = RequestProcessorServer.new(@stream, @context)
+      @event_processor = EventProcessor.new(@stream)
       @capture_file = File.open("#{Rails.root}/capture-#{$$}.log", "w") if ENV['LOGJAM_CAPTURE']
     end
 
@@ -38,7 +39,8 @@ module Logjam
       @socket = @context.socket(ZMQ::SUB)
       @socket.setsockopt(ZMQ::LINGER, 500)
       @socket.setsockopt(ZMQ::RCVHWM, 5000)
-      @socket.setsockopt(ZMQ::SUBSCRIBE, subscription_key)
+      @socket.setsockopt(ZMQ::SUBSCRIBE, requests_subscription_key)
+      @socket.setsockopt(ZMQ::SUBSCRIBE, events_subscription_key)
       @stream.importer.hosts.each do |host|
         address = "tcp://#{host}:9606"
         log_info "connecting to #{address}"
@@ -48,33 +50,47 @@ module Logjam
         key = p1.copy_out_string; p1.close
         msg = p2.copy_out_string; p2.close
         # puts key, msg
-        process_request(msg)
+        case key
+        when /^logs/
+          process_request(msg)
+        when /^events/
+          process_event(msg)
+        end
       end
     end
 
     def shutdown
       stop_reparenting_timer
       log_info "shutting down zmq importer"
-      @socket.setsockopt(ZMQ::UNSUBSCRIBE, subscription_key)
+      @socket.setsockopt(ZMQ::UNSUBSCRIBE, requests_subscription_key)
       @socket.unbind
       log_info "stopping processor"
-      @processor.stop
+      @request_processor.stop
       EM.stop
       # exit immediately to avoid: Assertion failed: ok (mailbox.cpp:79)
       log_info "exiting zmq_worker"
       exit!(0)
     end
 
-    def subscription_key
+    def requests_subscription_key
       # TODO: should env really be part of the subscription key?
       ["logs", @application, @environment].compact.join('.')
+    end
+
+    def events_subscription_key
+      # TODO: should env really be part of the subscription key?
+      ["events", @application, @environment].compact.join('.')
     end
 
     def process_request(msg)
       (c = @capture_file) && (c.puts msg)
       request = Oj.load(msg, :mode => :compat)
-      @processor.process_request(request)
+      @request_processor.process(request)
     end
 
+    def process_event(msg)
+      event = Oj.load(msg, :mode => :compat)
+      @event_processor.process(event)
+    end
   end
 end
