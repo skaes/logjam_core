f704beb20904050899ffeff93d88b2634e7ec0f4
Stefan Kaes <stefan.kaes@xing.com>
2013-10-07 21:13:28 +0200

    basic iPhone and iPad support

--- app/assets/javascripts/logjam-header.js
+++ app/assets/javascripts/logjam-header.js
@@ -108,8 +108,10 @@ function initialize_header() {
      onSelect: function(value, data){ submit_filter_form(); }
    });
 
-  $("*[title]").tipsy({gravity:"nw", opacity:0.9, offset:10, delayIn:250, delayOut:0, fade:false});
-
+  var mobile = navigator.userAgent.match(/iPad|iPhone/i) != null;
+  if (!mobile) {
+    $("*[title]").tipsy({gravity:"nw", opacity:0.9, offset:10, delayIn:250, delayOut:0, fade:false});
+  }
   // bug in safari leaves the tooltips open and you can have more than one shown
   if ($.browser.safari) {
     $(window).on("beforeunload", function(e){ $(".tipsy").remove(); });
