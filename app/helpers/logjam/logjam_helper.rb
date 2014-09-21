d3a221cf090aa45d644aa9d8ae19ca47182a883e
Stefan Kaes <stefan.kaes@xing.com>
2014-09-21 10:43:59 +0200

    another layout change

--- app/helpers/logjam/logjam_helper.rb
+++ app/helpers/logjam/logjam_helper.rb
@@ -164,11 +164,11 @@ module Logjam
       end
     end
 
-    def sometimes_link_all_pages(text)
+    def sometimes_link_all_pages
       if params[:grouping] == "page"
-        clean_link_to("show all actions", { :action => "totals_overview", :page => @page }, :title => "show all actions")
+        clean_link_to("show all actions", { :action => "totals_overview", :page => @page }, :title => "show all actions", :style => 'float:right;')
       elsif params[:grouping] == "request"
-        clean_link_to("browse requests", { :action => "request_overview", :page => @page }, :title => "browse requests")
+        clean_link_to("browse requests", { :action => "request_overview", :page => @page }, :title => "browse requests", :style => 'float:right;')
       end
     end
 
