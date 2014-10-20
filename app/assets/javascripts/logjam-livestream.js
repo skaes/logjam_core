7356bf967e2d726a14a4cd3865523b9b563e6fdc
Stefan Kaes <stefan.kaes@xing.com>
2014-10-20 11:17:14 +0200

    don't disconnect live stream on resizing

--- app/assets/javascripts/logjam-livestream.js
+++ app/assets/javascripts/logjam-livestream.js
@@ -436,13 +436,13 @@ function logjam_live_stream_chart(params){
 
   /* automatically connect to the data stream when the ducoment is ready */
   $(function(){
+    console.log(ws);
     $("#stream-toggle").on("click", function(){ toggle_stream($(this)); });
     $("#warnin-toggle").on("click", function(){ toggle_warnings($(this)); });
     $("#smooth-toggle").on("click", function(){ toggle_smoothness($(this)) ;});
     connect_chart();
 
     $(window).on('resize', function(){
-      disconnect_chart();
       $('#live-stream-chart svg').remove();
       logjam_live_stream_chart(params);
     });
