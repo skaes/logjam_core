2b59bd67c745a956515d48d54815f8b510023109
Stefan Kaes <stefan.kaes@xing.com>
2015-08-23 17:45:55 +0200

    pick front-end timing endpoint protocol from request

--- app/helpers/logjam/logjam_helper.rb
+++ app/helpers/logjam/logjam_helper.rb
@@ -16,7 +16,7 @@ module Logjam
     end
 
     def logjam_header_timings_collector
-      collector_url = Logjam.frontend_timings_collector || "http://#{request.host}:#{Logjam.frontend_timings_collector_port}"
+      collector_url = Logjam.frontend_timings_collector || "#{request.protocol}#{request.host}:#{Logjam.frontend_timings_collector_port}"
       tag(:meta, :name => "logjam-timings-collector", :content => collector_url)
     end
 
